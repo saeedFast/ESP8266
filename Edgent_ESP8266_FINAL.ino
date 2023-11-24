1330ecd34fa9f67a524788a5bfb3dddb62ed3a32
saeedFast <149888927+saeedFast@users.noreply.github.com>
2023-11-24 14:47:38 +0500

    Update Edgent_ESP8266_FINAL.ino

--- Edgent_ESP8266_FINAL.ino
+++ Edgent_ESP8266_FINAL.ino
@@ -1,26 +1,5 @@
-/*************************************************************
-  Blynk is a platform with iOS and Android apps to control
-  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build mobile and web interfaces for any
-  projects by simply dragging and dropping widgets.
+//Controlling LED through Blynk app via WiFi on NodeMCU8266
 
-    Downloads, docs, tutorials: https://www.blynk.io
-    Sketch generator:           https://examples.blynk.cc
-    Blynk community:            https://community.blynk.cc
-    Follow us:                  https://www.fb.com/blynkapp
-                                https://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
- *************************************************************
-  Blynk.Edgent implements:
-  - Blynk.Inject - Dynamic WiFi credentials provisioning
-  - Blynk.Air    - Over The Air firmware updates
-  - Device state indication using a physical LED
-  - Credentials reset using a physical Button
- *************************************************************/
-
-/* Fill in information from your Blynk Template here */
-/* Read more: https://bit.ly/BlynkInject */
 #define BLYNK_TEMPLATE_ID "TMPL6pcC4rN-O"
 #define BLYNK_TEMPLATE_NAME "LED test CHY9"
 //#define BLYNK_AUTH_TOKEN "nQX7WW4f79UrBo2aI9uADbB4sg2vOpF6"
